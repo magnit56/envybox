d9164b4af6228ff84ac5308ed720f35348d789f7
Тулегенов Самат Булатович <tulegenov.s_qschool@qsoft.ru>
2022-05-22 11:17:12 +0500

    Добавлена валидация на фронтенде

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+import BootstrapVue from "bootstrap-vue";
 
 window.Vue = require('vue').default;
 
@@ -20,6 +21,8 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+Vue.use(BootstrapVue);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,29 +1,33 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <form class="col-md-8" @submit.prevent="onSubmit">
-                <div class="form-group">
-                    <label for="name">Имя</label>
-                    <input type="text" class="form-control" id="name" placeholder="Alex" v-model="form.name">
-                </div>
-                <div class="form-group">
-                    <label for="phone">Номер телефона</label>
-                    <input id="phone" class="form-control" name="phone" type="text" placeholder="88005553535" v-model="form.phone">
-                </div>
-                <div class="form-group">
-                    <label for="message">Текст обращения</label>
-                    <textarea class="form-control" id="message" rows="3" placeholder="Мое сообщение" v-model="form.message"></textarea>
-                </div>
+            <b-form class="col-md-8" @submit.stop.prevent="onSubmit">
+                <b-alert variant="success" show v-if="submitted">Сообщение отправлено</b-alert>
+                <b-form-group id="name-group" label="Имя" label-for="name">
+                    <b-form-input type="text" class="form-control" id="name" name="name" placeholder="Alex" v-model="$v.form.name.$model" :state="validateState('name')" aria-describedby="name-live-feedback"></b-form-input>
+                    <b-form-invalid-feedback id="name-live-feedback">Обязательное поле</b-form-invalid-feedback>
+                </b-form-group>
+
+                <b-form-group id="phone-group" label="Телефон" label-for="phone">
+                    <b-form-input type="text" class="form-control" id="phone" name="phone" placeholder="88005553535" v-model="$v.form.phone.$model" :state="validateState('phone')" aria-describedby="phone-live-feedback"></b-form-input>
+                    <b-form-invalid-feedback id="phone-live-feedback">В поле должен быть номер телефона в формате 88005553535</b-form-invalid-feedback>
+                </b-form-group>
+
+                <b-form-group id="message-group" label="Текст обращения" label-for="message">
+                    <b-form-textarea class="form-control" id="message" rows="3" placeholder="Мое сообщение" v-model="$v.form.message.$model" :state="validateState('message')" aria-describedby="message-live-feedback"></b-form-textarea>
+                    <b-form-invalid-feedback id="message-live-feedback">Обязательное поле</b-form-invalid-feedback>
+                </b-form-group>
                 <button class="btn btn-primary" type="submit">Отправить обращение</button>
-            </form>
+            </b-form>
         </div>
     </div>
 </template>
 
 <script>
-    // import { required, email } from '@vuelidate/validators';
-    // import { useVuelidate } from '@vuelidate/core';
+    import { validationMixin } from "vuelidate";
+    import { required, minLength, maxLength, numeric } from "vuelidate/lib/validators";
     export default {
+        mixins: [validationMixin],
         data() {
             return {
                 form: {
@@ -31,30 +35,68 @@
                     phone: null,
                     message: null,
                 },
-                flash: null,
+                submitted: false,
             };
+        },
+        validations: {
+            form: {
+                name: {
+                    required,
+                },
+                phone: {
+                    required,
+                    numeric,
+                    minLength: minLength(11),
+                    maxLength: maxLength(11),
+                },
+                message: {
+                    required,
+                }
+            }
         },
         mounted() {
             console.log('Component mounted.')
         },
         methods: {
-            async onSubmit() {
-                // console.log('submitted');
-                // console.log(this.form);
-                await axios.post('/api', this.form)
+            onSubmit() {
+                this.$v.form.$touch();
+                if (this.$v.form.$anyError) {
+                    return;
+                }
+                return axios.post('/api', this.form)
                     .then(response => console.log(response.data))
-                    .catch(error => console.log(error));
-            }
+                    .catch(error => console.log(error))
+                    .then(() => this.resetForm())
+                    .then(() => this.submitted = true);
+            },
+            resetForm() {
+                this.form = {
+                    name: null,
+                    phone: null,
+                    message: null,
+                };
+
+                this.resetFlash();
+
+                this.$nextTick(() => {
+                    this.$v.$reset();
+                });
+            },
+            resetFlash() {
+                this.submitted = false;
+            },
+            validateState(name) {
+                const { $dirty, $error } = this.$v.form[name];
+                return $dirty ? !$error : null;
+            },
         },
-        // setup: () => ({ v$: useVuelidate() }),
-        // validations () {
-        //     return {
-        //         form: {
-        //             name: { required, email },
-        //             phone: { required },
-        //             message: { required },
-        //         }
-        //     }
-        // }
+        watch: {
+            form: {
+                handler (val, oldVal) {
+                    this.submitted = false;
+                },
+                deep: true,
+            },
+        },
     }
 </script>
